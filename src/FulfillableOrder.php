a751711640ff0e0e45e1094cd48befbf7b32e8f0
XKR STUDIO <xkrstudio@gmail.com>
2022-04-10 11:07:14 +0200

    ADD: validateJSON and test. CHANGE: end of new line of files.

--- src/FulfillableOrder.php
+++ src/FulfillableOrder.php
@@ -1,9 +1,17 @@
 <?php
 class FulfillableOrder {
+    private ?object $stock;
 
     public function checkParameter(int $parameter) : void {
         if ($parameter != 2) {
             echo 'Ambiguous number of parameters!';
+            exit(1);
+        }
+    }
+
+    public function validateJSON(string $json) : void {
+        if (($this->stock = json_decode($json)) == null) {
+            echo 'Invalid json!';
             exit(1);
         }
     }
--- tests/FulFillableOrderTest.php
+++ tests/FulFillableOrderTest.php
@@ -16,6 +16,14 @@ class FulfillableOrderTest extends TestCase {
     public function testCheckParameter() : void {
         $this->expectOutputString('Ambiguous number of parameters!', $this->fulfillableOrder->checkParameter(0));
     }
+
+    public function testValidJSON() : void {
+        $this->assertEquals('', $this->fulfillableOrder->validateJSON('{"1":8,"2":4,"3":5}'));
+    }
+
+    public function testInvalidJSON() : void {
+        $this->expectOutputString('Invalid json!', $this->fulfillableOrder->validateJSON('invalid data'));
+    }
     
 }
 ?>
